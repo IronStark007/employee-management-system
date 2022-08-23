486de3e013562b05a3bca364a4426df17679da34
Mohd Ali Ansari <ansar326629@gmail.com>
2022-08-23 16:47:11 +0530

    removed unsed console

--- db.js
+++ db.js
@@ -10,7 +10,6 @@ const client = new Client({
 })
 client.connect(err=>{
     if (err){
-        console.log("erroro connecting to databse")
         console.log(err);
     }else {
         console.log("Connected to database!")
